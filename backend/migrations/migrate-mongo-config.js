c4061a0dea5b817cc2901147e12cef1a57d8feef
roman <rossraijin@gmail.com>
2023-12-03 13:18:27 +0200

    hotfix

--- backend/migrations/migrate-mongo-config.js
+++ backend/migrations/migrate-mongo-config.js
@@ -3,7 +3,7 @@
 const config = {
   mongodb: {
     // TODO Change (or review) the url to your MongoDB:
-    url: "mongodb://localhost:27017",
+    url: "mongodb://127.0.0.1:27017",
 
     // TODO Change this to your database name:
     databaseName: "bookstore",
