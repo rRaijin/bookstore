2f744b9e119cf0b20008eeaa6fe9cd80394a8498
Svyatoslav <savchuksr@gmail.com>
2024-07-07 08:10:21 +0300

    07.07.24

--- frontend/src/components/SampleForm.jsx
+++ frontend/src/components/SampleForm.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react';
-
 import Select from './elements/Select';
 import InputGeneral from './fields/InputGeneral';
 import TextareaField from './fields/TextareaField';
 import Captcha from './fields/Captcha';
 
-
 const SampleForm = (props) => {
-    const {  } = props;
+    const { } = props;
 
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
@@ -15,68 +13,69 @@ const SampleForm = (props) => {
     const [allowed, setAllowedStatus] = useState(false);
     const [captchavisible, changeStatus] = useState(false);
 
-    // после заполнения всех полей при нажатии на отправку візвать капчу, 
-    // если капча пройдена віполнить отправку на сервер, капчу скріть
-
-    // сделать капчу в двух режимах: 1й как сейчас, 2й - "HARD MODE"
-
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('form submitted DATA: ', title, description, category);
         changeStatus(true);
-        // if (allowed) {
-        //     const data = {
-        //         title,
-        //         description,
-        //         cat: category.id
-        //     };
-        // console.log(data)
-        //     // send to server
-        // } else {
+    };
 
-        // }
-    }
+    const handleCaptchaComplete = (status) => {
+        if (status) {
+            const data = {
+                title,
+                description,
+                cat: category.id
+            };
+            console.log('Captcha passed, form data: ', data);
+            changeStatus(false); // прячу капчу после ее прохождения
+        }
+    };
 
     return (
         <form onSubmit={handleSubmit}>
             <Select
-                items={
-                    [
-                        {id: 1, name: 'bread'},
-                        {id: 2, name: 'milk'},
-                        {id: 3, name: 'apple'},
-                        {id: 4, name: 'cheese'},
-                        {id: 5, name: 'meet'}
-                    ]
-                }
+                items={[
+                    { id: 1, name: 'bread' },
+                    { id: 2, name: 'milk' },
+                    { id: 3, name: 'apple' },
+                    { id: 4, name: 'cheese' },
+                    { id: 5, name: 'meat' }
+                ]}
                 propToView='name'
                 initialId={2}
-                upToParent={setCategory}/>
+                upToParent={setCategory}
+            />
 
             <InputGeneral
                 inputType='text'
                 value={title}
                 onChangeHandle={e => setTitle(e.target.value)}
-                className={''}/>
+                className={''}
+            />
             <InputGeneral
                 inputType='text'
                 value={description}
                 onChangeHandle={e => setDescription(e.target.value)}
-                className={''}/>
+                className={''}
+            />
             <TextareaField
                 className=''
                 initialValue='Some text'
                 fieldName='description'
-                onChangeHandler={() => console.log('change')}/>
+                onChangeHandler={() => console.log('change')}
+            />
             {
                 captchavisible &&
-                <Captcha/>
+                <Captcha onCaptchaComplete={handleCaptchaComplete} />
             }
-            <button type='submit'>
-                Калькуляция
-            </button>
+            {
+                !captchavisible && !allowed && (
+                    <button type='submit'>
+                        Калькуляция
+                    </button>
+                )
+            }
         </form>
-    )
-}
+    );
+};
 
 export default SampleForm;
--- frontend/src/components/fields/Captcha.jsx
+++ frontend/src/components/fields/Captcha.jsx
@@ -3,97 +3,98 @@ import React, { useState, useEffect } from 'react';
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const evenNumbers = [2, 4, 6, 8];
 const images = [
-  { src: '/bg-pictures/cute-cat.jpg', isCat: true },
-  { src: '/bg-pictures/honey.jpg', isCat: false },
-  { src: '/bg-pictures/cute-cat.jpg', isCat: true },
-  { src: '/bg-pictures/sausage.jpg', isCat: false },
-  { src: '/bg-pictures/cute-cat.jpg', isCat: true },
-  { src: '/bg-pictures/house.jpg', isCat: false },
-  { src: '/bg-pictures/cute-cat.jpg', isCat: true },
-  { src: '/bg-pictures/honey.jpg', isCat: false },
-  { src: '/bg-pictures/cute-cat.jpg', isCat: true },
+    { src: '/bg-pictures/cute-cat.jpg', isCat: true },
+    { src: '/bg-pictures/honey.jpg', isCat: false },
+    { src: '/bg-pictures/cute-cat.jpg', isCat: true },
+    { src: '/bg-pictures/sausage.jpg', isCat: false },
+    { src: '/bg-pictures/cute-cat.jpg', isCat: true },
+    { src: '/bg-pictures/house.jpg', isCat: false },
+    { src: '/bg-pictures/cute-cat.jpg', isCat: true },
+    { src: '/bg-pictures/honey.jpg', isCat: false },
+    { src: '/bg-pictures/cute-cat.jpg', isCat: true },
 ];
 
-const Captcha = () => {
+const Captcha = ({ onCaptchaComplete }) => {
     const [level, setLevel] = useState(1);
     const [selected, setSelected] = useState([]);
     const [error, setError] = useState('');
-  
+
     useEffect(() => {
-      setLevel(1);
-      setSelected([]);
-      setError('');
+        setLevel(1);
+        setSelected([]);
+        setError('');
     }, []);
-  
+
     const handleNumberClick = (number) => {
-      setSelected((prev) => 
-        prev.includes(number) ? prev.filter(n => n !== number) : [...prev, number]
-      );
+        setSelected((prev) => 
+            prev.includes(number) ? prev.filter(n => n !== number) : [...prev, number]
+        );
     };
-  
+
     const handleImageClick = (index) => {
-      setSelected((prev) => 
-        prev.includes(index) ? prev.filter(i => i !== index) : [...prev, index]
-      );
+        setSelected((prev) => 
+            prev.includes(index) ? prev.filter(i => i !== index) : [...prev, index]
+        );
     };
-  
+
     const handleSubmit = () => {
-      if (level === 1) {
-        const isCorrect = selected.sort().toString() === evenNumbers.sort().toString();
-        if (isCorrect) {
-          setLevel(2);
-          setSelected([]);
-          setError('');
-        } else {
-          setError('Пожалуйста выберите все четные числа.');
+        if (level === 1) {
+            const isCorrect = selected.sort().toString() === evenNumbers.sort().toString();
+            if (isCorrect) {
+                setLevel(2);
+                setSelected([]);
+                setError('');
+            } else {
+                setError('Пожалуйста выберите все четные числа.');
+            }
+        } else if (level === 2) {
+            const isCorrect = selected.every(index => images[index].isCat) && selected.length === 5;
+            if (isCorrect) {
+                alert('Калькуляция отправлена в console');
+                onCaptchaComplete(true); // Вызов функции при успешном прохождении капчи
+            } else {
+                setError('Пожалуйста выберите все изображения с котиком.');
+            }
         }
-      } else if (level === 2) {
-        const isCorrect = selected.every(index => images[index].isCat) && selected.length === 5;
-        if (isCorrect) {
-          alert('Капча пройдена!');
-        } else {
-          setError('Пожалуйста выберите все изображения с котиком.');
-        }
-      }
     };
-  
+
     return (
-      <div>
-        {level === 1 ? (
-          <div>
-            <h2>Выберите все четные числа</h2>
-            <div className="grid">
-              {numbers.map((number) => (
-                <div
-                  key={`${number}`}
-                  className={`square ${selected.includes(number) ? 'selected' : ''}`}
-                  onClick={() => handleNumberClick(number)}
-                >
-                  {number}
+        <div>
+            {level === 1 ? (
+                <div>
+                    <h2>Выберите все четные числа</h2>
+                    <div className="grid">
+                        {numbers.map((number) => (
+                            <div
+                                key={`${number}`}
+                                className={`square ${selected.includes(number) ? 'selected' : ''}`}
+                                onClick={() => handleNumberClick(number)}
+                            >
+                                {number}
+                            </div>
+                        ))}
+                    </div>
                 </div>
-              ))}
-            </div>
-          </div>
-        ) : (
-          <div>
-            <h2>Выберите все изображения с котиком</h2>
-            <div className="grid">
-              {images.map((image, index) => (
-                <div
-                  key={index}
-                  className={`square ${selected.includes(index) ? 'selected' : ''}`}
-                  onClick={() => handleImageClick(index)}
-                >
-                  <img src={image.src} alt={`captcha-${index}`} />
+            ) : (
+                <div>
+                    <h2>Выберите все изображения с котиком</h2>
+                    <div className="grid">
+                        {images.map((image, index) => (
+                            <div
+                                key={index}
+                                className={`square ${selected.includes(index) ? 'selected' : ''}`}
+                                onClick={() => handleImageClick(index)}
+                            >
+                                <img src={image.src} alt={`captcha-${index}`} />
+                            </div>
+                        ))}
+                    </div>
                 </div>
-              ))}
-            </div>
-          </div>
-        )}
-        {error && <p className="error">{error}</p>}
-        <button onClick={handleSubmit}>Сохранить</button>
-      </div>
+            )}
+            {error && <p className="error">{error}</p>}
+            <button onClick={handleSubmit}>Сохранить</button>
+        </div>
     );
-  };
-  
-  export default Captcha;
+};
+
+export default Captcha;
