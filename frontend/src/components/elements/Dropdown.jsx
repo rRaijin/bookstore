1da149482d1416cad04c5d383672062bd565d36b
Svyatoslav <savchuksr@gmail.com>
2024-09-28 19:01:07 +0300

    28.09.24

--- frontend/src/components/elements/Dropdown.jsx
+++ frontend/src/components/elements/Dropdown.jsx
@@ -27,39 +27,49 @@ const Dropdown = (props) => {
     //     }
     // }, [initialId]);
 
-    // console.log('items: ', items);
+    console.log('items: ', items);
 
     // изначально показываем 1го автора, при наведении курсора мыши - открываем весь список(можно без скрола, но лучше с ним),
     // клик по автору - меняем стейт и закрываем список, желательно css красиво
 
     return (
         <div className={className}
-            // onMouseEnter={() => setSelectStatus(true)}
-            // onMouseLeave={() => setSelectStatus(false)}
+            onMouseEnter={() => setSelectStatus(true)}
+            onMouseLeave={() => setSelectStatus(false)}
         >
-            <div className=''>
-                <div className='selected-name'>
+            <div className='flex select-author-default'>
+                <div className=''>
                     {
-                        selectedItem && 
-                        selectedItem.firstName + ' ' + selectedItem.lastName
+                        selectedItem ? 
+                        `${selectedItem.firstName} ${selectedItem.lastName}` : 
+                        (items && items.length > 0 ? `${items[0].firstName} ${items[0].lastName}` : 'Нет авторов')
+
                     }
                 </div>
-                
+                <div>
+                    <div className={`arrow ${selectStatus ? 'open' : ''}`}>
+                        ▼
+                    </div>
+                </div>
             </div>
             {
                 <div className=''>
-                    {
-                        items.map((item, i) => {
-                            return (
-                                <div
-                                    className='select-item'
-                                    key={`${item.id}`}
-                                    onClick={() => onSelectHandle(item.id)}>
-                                    {item.firstName + ' ' + item.lastName}
-                                </div>
-                            )
-                        })
-                    }
+                    {selectStatus && items && items.length > 0 &&(
+                        <div className='authors-select'>
+                            {
+                                items.map((item, i) => {
+                                    return (
+                                        <div
+                                            className='author-select'
+                                            key={`${item.id}`}
+                                            onClick={() => onSelectHandle(item.id)}>
+                                            {item.firstName + ' ' + item.lastName}
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    )}
                 </div>
             }
         </div>
